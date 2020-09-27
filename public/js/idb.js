0201a039a5626c6371f869a58587dc75d1b7bc0a
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-09-27 15:15:06 -0500

    done with offline functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,7 +7,7 @@ request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
     // create an object store (table) called `new_transaction`
-    db.createObjectStore('new_pizza', { autoIncrement: true });
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 //successful connection
@@ -22,4 +22,53 @@ request.onsuccess = function(event) {
   request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
+
+//save offline transaction to indexDb
+function saveTransaction(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    transactionObjectStore.add(record);
+};
+
+//upload offline transactions
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    const getAllTransactions = transactionObjectStore.getAll();
+
+    getAllTransactions.onsuccess = function() {
+        if (getAllTransactions.result.length > 0) {
+          fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAllTransactions.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+              const transaction = db.transaction(['new_transaction'], 'readwrite');
+              const transactionObjectStore = transaction.objectStore('new_transaction');
+              transactionObjectStore.clear();
+    
+              alert('All transactions have been submitted!');
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+    };
+
+};
+
+//listen for app coming back online
+window.addEventListener('online', uploadTransaction);
 
